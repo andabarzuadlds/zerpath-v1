006fe87aca3b18e143d7ba81a348a622f7be3ace
andabarzuadlds <and.abarzua@dlds.cl>
2025-08-06 16:09:02 -0400

    Files Required

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import GameBoard from "./components/GameBoard";
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <GameBoard />
+    </div>
+  );
+}
+
+export default App;
