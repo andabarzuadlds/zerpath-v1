3082f27b23c667e8a03b8590c2f6c95e687a77a8
andabarzuadlds <and.abarzua@dlds.cl>
2025-08-07 09:58:23 -0400

    Import Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import GameBoard from "./components/GameBoard";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       <GameBoard />
+      <Analytics />
     </div>
   );
 }
